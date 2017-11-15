0e3e85a681413e940a5ec5f61f6906b5a4c30047
zomlandlin <zomland.lin@imediplus.com>
2017-11-15 17:46:01 +0800

    key update

--- testAPI/APIToTest.js
+++ testAPI/APIToTest.js
@@ -30,12 +30,12 @@ function HcInTest(req, res, next){
 
 function ccTest(req, res, next){
 
-	const nonce = web3.eth.getTransactionCount("0xe70C2ee30cbF71f92D6C3bC6153Fa588046b84D7");
+	const nonce = web3.eth.getTransactionCount("0xe3cd434465b88D8e3Ec057787270cCb6D3172698");
 
 	const nonceHex = web3.toHex(nonce);
 
 	request.post(
-		'http://192.168.51.202:3200/topChain/CC_signInformation/54dee1a12baaccb5589e062aa59bf72b95f689d260665770073bc095cc7c7e7c/{ "nonce": "'+nonceHex+'", "gasLimit": "0x5208", "to":"0x00765c5d8a2b57b75d77a77b85ff10898168cac4", "value": 1, "gasPrice": "0x05F5E100" } ',
+		'http://192.168.51.202:3200/topChain/CC_signInformation/4ed4eb8e21b4a180881a71956e3921c090102a7210fe3dd92d48ff2e76251315/{ "nonce": "'+nonceHex+'", "gasLimit": "0x5208", "to":"0x00765c5d8a2b57b75d77a77b85ff10898168cac4", "value": 1, "gasPrice": "0x5F5E100" } ',
 		function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				console.log("\n2.CC_signInformation:\n")
