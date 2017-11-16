ad3c4f9a850771e21afc0889d33d46223d56083e
zomlandlin <zomland.lin@imediplus.com>
2017-11-16 09:11:44 +0800

    key repair

--- testAPI/APIToTest.js
+++ testAPI/APIToTest.js
@@ -11,7 +11,6 @@ module.exports = {
 	}
 }
 function HcInTest(req, res, next){
-
 	request.post(
     	'http://192.168.51.202:3200/topChain/HC_signInformationIn/0x6a2f20a64dc0f784195db570ac14b2d2359fdb88/210000/26/1/20000000000" } ',
     	{ json: { key: 'value' } },
@@ -30,12 +29,12 @@ function HcInTest(req, res, next){
 
 function ccTest(req, res, next){
 
-	const nonce = web3.eth.getTransactionCount("0x87f1f23Db86D19256289F6950cBAF62E1c04BB96");
+	const nonce = web3.eth.getTransactionCount("0x9Bf2dBACE6533Dabd88fE2d2A2A7Be10EA8Cb995");
 
 	const nonceHex = web3.toHex(nonce);
 
 	request.post(
-		'http://192.168.51.202:3200/topChain/CC_signInformation/565984fcd24d0a84300143659e3e1295cc977ba71ec1bb961049df6cd7f3c7f9/{ "nonce": "'+nonceHex+'", "gasLimit": "0x5208", "to":"0x00765c5d8a2b57b75d77a77b85ff10898168cac4", "value": 1, "gasPrice": "0x1" } ',
+		'http://127.0.0.1:3200/topChain/CC_signInformation/07c97e0a6a71d9b707f1eb9d89a2c0dd40904a5baf7f227f938b95460dce32ba/{ "nonce": "'+nonceHex+'", "gasLimit": "0x5208", "to":"0x00765c5d8a2b57b75d77a77b85ff10898168cac4", "value": 1, "gasPrice": "0x1" } ',
 		function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				console.log("\n2.CC_signInformation:\n")
@@ -54,7 +53,7 @@ function ccTest(req, res, next){
 
 function HcOutTest(req, res, next, tx){
 	request.post(
-		'http://192.168.51.202:3200/topChain/HC_signInformationOut/'+tx,
+		'http://127.0.0.1:3200/topChain/HC_signInformationOut/'+tx,
 		function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				console.log("\n3.CC_signInformation:\n")
